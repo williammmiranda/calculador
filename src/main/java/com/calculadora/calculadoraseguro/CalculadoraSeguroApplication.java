03e3a4d99eec161216fa38494785417cc8028b23
William Miranda <wmendes.miranda@gmail.com>
2023-12-07 15:23:43 -0300

    Configurações iniciais

---
+++ src/main/java/com/calculadora/calculadoraseguro/CalculadoraSeguroApplication.java
@@ -0,0 +1,14 @@
+package com.calculadora.calculadoraseguro;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraSeguroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraSeguroApplication.class, args);
+	}
+
+}
